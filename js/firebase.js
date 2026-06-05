d9aa89723e86a377bcd08efb36286edc33cea406
Kun Jut <kunjut.ua@gmail.com>
2026-06-05 23:18:08 +0300

    refactor: format firebaseConfig for improved readability

--- js/firebase.js
+++ js/firebase.js
@@ -7,12 +7,13 @@ import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/
 import { getFirestore }        from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey:            "AIzaSyD71Jf3fEB0LPzYR4-D0ktdItK5WXbcCko",
-  authDomain:        "fitforge-c8e54.firebaseapp.com",
-  projectId:         "fitforge-c8e54",
-  storageBucket:     "fitforge-c8e54.firebasestorage.app",
+  apiKey: "AIzaSyD71Jf3fEB0lPzYR4-D0ktdItK5WXbcCko",
+  authDomain: "fitforge-c8e54.firebaseapp.com",
+  projectId: "fitforge-c8e54",
+  storageBucket: "fitforge-c8e54.firebasestorage.app",
   messagingSenderId: "1092241300162",
-  appId:             "1:1092241300162:web:1a1fedec2b96bb627d395b"
+  appId: "1:1092241300162:web:1a1fedec2b96bb627d395b",
+  measurementId: "G-0SKBBQDSDC"
 };
 
 const app = initializeApp(firebaseConfig);
